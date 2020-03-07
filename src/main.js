c02d60d1028feef66aef1862978b3d1fab867e82
j-pedro13 <joao@agys.com.br>
2020-03-07 08:27:02 -0300

    firebase

--- src/main.js
+++ src/main.js
@@ -10,5 +10,17 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyC-1nKYK_5HYn11g9AiP5BL0RqO6MUYfcM",
+      authDomain: "global-legal-hack.firebaseapp.com",
+      databaseURL: "https://global-legal-hack.firebaseio.com",
+      projectId: "global-legal-hack",
+      storageBucket: "global-legal-hack.appspot.com",
+      messagingSenderId: "1057686815977",
+      appId: "1:1057686815977:web:ac5c00c4387f7fc9d7dc78"  
+    })
+  }
 }).$mount('#app')
+  
